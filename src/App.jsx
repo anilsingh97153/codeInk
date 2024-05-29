7792b71556d0bbae0416e48173c84794ab19a449
anilsingh97153 <anilsuyal97153@gmail.com>
2024-05-29 18:09:19 +0530

    style updated.

--- src/App.jsx
+++ src/App.jsx
@@ -38,8 +38,8 @@ function App() {
 
   useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: ['<strong>CODEINK</strong>', 'Code editor to practice HTML, CSS &amp; Javascript.', 'Save all your code in local storage.'],
-      typeSpeed: 40,
+      strings: ['<strong>CODEINK &lt; / &gt;</strong>', 'Code editor to practice HTML, CSS &amp; Javascript.', 'Save all your code in local storage.', '<strong>CODEINK &lt; / &gt;</strong>'],
+      typeSpeed: 50,
     });
 
     return () => {
@@ -49,7 +49,7 @@ function App() {
   }, []);
 
   return (
-    <div className="overflow-hidden bg-blue-100">
+    <div className="overflow-hidden bg-white">
       <nav className='0 text-white p-2 w-screen bg-blue-950	fixed z-40'><span ref={el} /></nav>
      {/* Element to contain animated typing  */}
   <span id="element"></span>
@@ -79,63 +79,27 @@ function App() {
             onChange={setJsCode}
           /></div>
 
-        <div className="sm:col-span-3 bg-sky-950	">
+        <div className="rounded-t-lg sm:col-span-3"><h6 className='text-white font-semibold bg-black px-3 py-0.5 rounded-t-lg'>OUTPUT</h6>
           <iframe
             title='output'
             srcDoc={srcDoc}
             sandbox='allow-scripts allow-modals'
             height="100%"
             width="100%"
-            style={{ width: '100%', height: '100%', border: 'none', overflow: 'hidden', boxSizing: 'border-box' }}
+            style={{width: '100%', height: '100%', border: 'none', overflow: 'hidden', boxSizing: 'border-box'}}
+            onLoad={(event) => {
+              const iframeDocument = event.target.contentDocument;
+              if (iframeDocument) {
+                const iframeBody = iframeDocument.querySelector('body');
+                if (iframeBody) {
+                  iframeBody.style.color = 'white'; // Set text color to white
+                }
+              }
+            }}
           />
         </div>
       </div>
     </div>
-
-
-    // <div className="app">
-    //   {/* code pane */}
-
-    //   <div className="code-pane">
-    //     <div style={{ height: "100%" }}>
-    //       <Editor
-    //         value={htmlCode}
-    //         editorName="HTML"
-    //         languageExtension={html()}
-    //         onChange={setHtmlCode}
-    //       />
-    //     </div>
-    //     <div style={{ height: "100%", overflow: "hidden" }}>
-    //       <Editor
-    //         value={cssCode}
-    //         editorName="CSS"
-    //         languageExtension={css()}
-    //         onChange={setCssCode}
-    //       />
-    //     </div>
-    //     <div style={{ height: "100%" }}>
-    //       <Editor
-    //         value={jsCode}
-    //         editorName="JS"
-    //         languageExtension={javascript({ jsx: true })}
-    //         onChange={setJsCode}
-    //       />
-    //     </div>
-    //   </div>
-
-    //   {/* output pane */}
-
-    //   <div className="output-pane">
-    //     <iframe
-    //       title='output'
-    //       srcDoc={srcDoc}
-    //       sandbox='allow-scripts allow-modals'
-    //       height="100%"
-    //       width="100%"
-    //       style={{ width: '100%', height: '100%', border: 'none', overflow: 'hidden', boxSizing: 'border-box' }}
-    //     />
-    //   </div>
-    // </div>
 
   )
 }
